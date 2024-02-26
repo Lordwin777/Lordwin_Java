59915b888f637d85a07926926cfd6135323b3930
Lordwin777 <159879462+Lordwin777@users.noreply.github.com>
2024-02-26 14:25:05 +0530

    Add files via upload

---
+++ String_User.java
@@ -0,0 +1,11 @@
+public class String_User
+{
+    public static void main(String[] args) 
+    {
+        String greeting1 = new String("Hello \n");
+        System.out.println(" "+greeting1);
+        
+		String greeting2 = "Hello World";
+        System.out.println(" "+greeting2);
+	}
+}
