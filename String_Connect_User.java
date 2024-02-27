09e715af8cbd338d4300ac186b5a880c3b2b36f1
Lordwin777 <159879462+Lordwin777@users.noreply.github.com>
2024-02-27 13:03:24 +0530

    Add files via upload

---
+++ String_Connect_User.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class String_Connect_User
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        String str1;
+        str1 = sc.nextLine();
+        System.out.println(""+str1);
+        
+        String str2;
+        str2 = sc.nextLine();
+        System.out.println(" \n"+str2);
+
+        System.out.println(" \n"+str1+str2);
+		
+	}
+}
