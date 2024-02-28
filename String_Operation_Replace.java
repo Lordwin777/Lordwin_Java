6058ce2145533d77f03a6340ad34fb4705872d84
Lordwin777 <159879462+Lordwin777@users.noreply.github.com>
2024-02-28 15:39:40 +0530

    Add files via upload

---
+++ String_Operation_Replace.java
@@ -0,0 +1,10 @@
+public class String_Operation_Replace
+{
+    public static void main(String[] args) 
+    {
+        String str1 = "Java is Best";
+        String replaceString = str1.replace("Java","Lordwin");
+        System.out.println(replaceString);
+		
+	}
+}
