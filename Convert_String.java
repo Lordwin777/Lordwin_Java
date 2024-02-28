aad5bfa9587f727a0f53e2313b871aa4bfa66a15
Lordwin777 <159879462+Lordwin777@users.noreply.github.com>
2024-02-28 15:33:33 +0530

    Add files via upload

---
+++ Convert_String.java
@@ -0,0 +1,12 @@
+import java.lang.Integer;
+
+public class Convert_String
+{
+    public static void main(String[] args)
+    {
+        String s = "90";
+        int n = Integer.parseInt(s);
+        System.out.println("Equivalent Integer is: " +s);
+        
+    }
+}
